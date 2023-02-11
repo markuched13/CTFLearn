d8cd2211bd4142e969cd6bdbb2c707ac1cfa1266
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-11 06:14:15 +0100

    Update solve.py

--- pwn/Lazy Game Challenge/solve.py
+++ pwn/Lazy Game Challenge/solve.py
@@ -4,21 +4,25 @@ from time import sleep
 io = remote('thekidofarcrania.com', 10001)
 context.log_level = 'info'
 
-io.recvuntil('Are you ready? Y/N : ')
+print(io.recv().decode())
+
+#io.recvuntil('Are you ready? Y/N : ')
 io.sendline('Y')
 
 amount = 500
 target = 1100000
 win = amount - target
 
-io.recvuntil('Place a Bet :')
+#io.recvuntil('Place a Bet :')
+print(io.recv().decode())
 io.sendline(str(int(win)))
 
 #io.recvuntil('Make a Guess :')
 guess = ['0','0','0','0','0','0','0','0','0','0',]
 
 for i in guess:
-	sleep(1)
-	io.sendline(str(i))
+        print(io.recv())
+        sleep(1)
+        io.sendline(str(i))
 
 io.interactive()
